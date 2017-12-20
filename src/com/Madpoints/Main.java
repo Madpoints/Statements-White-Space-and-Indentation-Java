49ce0a902d96a27f3799503fa9c20a36de1b2649
John Furtado <jf8846@gmail.com>
2017-12-19 19:22:32 -0500

    Writing easy-to-read code in Java

---
+++ src/com/Madpoints/Main.java
@@ -0,0 +1,13 @@
+package com.Madpoints;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // entire line is a statement;
+        int variable = 50;
+        variable++;
+        System.out.println("This is " +
+                            "also an " +
+                            "expression");
+    }
+}
